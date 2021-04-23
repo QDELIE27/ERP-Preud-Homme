ed17ccbc578629d18eb67a7e58151d1dcc5ee2c5
Quentin Deliessche <quentin.deliessche@hotmail.com>
2021-04-23 17:23:50 +0200

    First init

---
+++ msrp3/src/main/java/org/banque/erp/App.java
@@ -0,0 +1,13 @@
+package org.banque.erp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
